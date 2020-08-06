0df45ca1bade81332fdfdb30cc0a269d91add92b
minuyim <minu_yim@naver.com>
2020-08-06 15:11:23 +0900

    test: 요금 기능 추가 확인을 위한 인수 테스트 수정

--- src/main/java/wooteco/subway/maps/map/dto/PathResponse.java
+++ src/main/java/wooteco/subway/maps/map/dto/PathResponse.java
@@ -1,13 +1,14 @@
 package wooteco.subway.maps.map.dto;
 
-import wooteco.subway.maps.station.dto.StationResponse;
-
 import java.util.List;
+
+import wooteco.subway.maps.station.dto.StationResponse;
 
 public class PathResponse {
     private List<StationResponse> stations;
     private int duration;
     private int distance;
+    private int fare;
 
     public PathResponse() {
     }
@@ -16,6 +17,13 @@ public class PathResponse {
         this.stations = stations;
         this.duration = duration;
         this.distance = distance;
+    }
+
+    public PathResponse(List<StationResponse> stations, int duration, int distance, int fare) {
+        this.stations = stations;
+        this.duration = duration;
+        this.distance = distance;
+        this.fare = fare;
     }
 
     public List<StationResponse> getStations() {
@@ -28,5 +36,9 @@ public class PathResponse {
 
     public int getDistance() {
         return distance;
+    }
+
+    public int getFare() {
+        return fare;
     }
 }
--- src/test/java/wooteco/subway/maps/map/acceptance/PathAcceptanceTest.java
+++ src/test/java/wooteco/subway/maps/map/acceptance/PathAcceptanceTest.java
@@ -1,19 +1,20 @@
 package wooteco.subway.maps.map.acceptance;
 
-import com.google.common.collect.Lists;
-import io.restassured.response.ExtractableResponse;
-import io.restassured.response.Response;
+import static wooteco.subway.maps.line.acceptance.step.LineStationAcceptanceStep.*;
+import static wooteco.subway.maps.map.acceptance.step.PathAcceptanceStep.*;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import com.google.common.collect.Lists;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import wooteco.subway.common.acceptance.AcceptanceTest;
 import wooteco.subway.maps.line.acceptance.step.LineAcceptanceStep;
 import wooteco.subway.maps.line.dto.LineResponse;
 import wooteco.subway.maps.station.acceptance.step.StationAcceptanceStep;
 import wooteco.subway.maps.station.dto.StationResponse;
-
-import static wooteco.subway.maps.line.acceptance.step.LineStationAcceptanceStep.지하철_노선에_지하철역_등록되어_있음;
-import static wooteco.subway.maps.map.acceptance.step.PathAcceptanceStep.*;
 
 @DisplayName("지하철 경로 조회")
 public class PathAcceptanceTest extends AcceptanceTest {
@@ -68,7 +69,6 @@ public class PathAcceptanceTest extends AcceptanceTest {
         총_거리와_소요_시간을_함께_응답함(response, 3, 4);
     }
 
-
     @DisplayName("두 역의 최소 시간 경로를 조회한다.")
     @Test
     void findPathByDuration() {
@@ -77,6 +77,27 @@ public class PathAcceptanceTest extends AcceptanceTest {
         //then
         적절한_경로를_응답(response, Lists.newArrayList(교대역, 강남역, 양재역));
         총_거리와_소요_시간을_함께_응답함(response, 4, 3);
+    }
+
+    @DisplayName("두 역의 최단 거리 경로를 요금과 함께 조회한다.")
+    @Test
+    void findPathByDistanceWithFare() {
+        //when
+        ExtractableResponse<Response> response = 거리_경로_조회_요청("DISTANCE", 1L, 3L);
+
+        //then
+        적절한_경로를_응답(response, Lists.newArrayList(교대역, 남부터미널역, 양재역));
+        총_거리와_소요_시간을_요금과_함께_응답함(response, 3, 4, 1250);
+    }
+
+    @DisplayName("두 역의 최소 시간 경로를 요금과 함께 조회한다.")
+    @Test
+    void findPathByDurationFare() {
+        //when
+        ExtractableResponse<Response> response = 거리_경로_조회_요청("DURATION", 1L, 3L);
+        //then
+        적절한_경로를_응답(response, Lists.newArrayList(교대역, 강남역, 양재역));
+        총_거리와_소요_시간을_요금과_함께_응답함(response, 3, 4, 2050);
     }
 
     private Long 지하철_노선_등록되어_있음(String name, String color) {
